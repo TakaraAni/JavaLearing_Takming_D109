e2c2e7473fd2f3cfbf8a9f013261230af2728ad5
TakaraAni <c324@kimo.com>
2021-12-27 14:08:26 +0800

    Create D10919211.java

---
+++ src/D10919211.java
@@ -0,0 +1,11 @@
+class D10919211{
+	public static void main(String ar[]) {
+		System.out.println("test for this");
+	}
+	
+	static String showMe() {
+		String MyName = "方寶瑜";
+		String StudenID = "D10919211";
+		return "My Name is "+MyName+"\nID:"+StudenID+"\n";
+	}
+}
