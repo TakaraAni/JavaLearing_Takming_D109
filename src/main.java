266c013784ab585d09229c66959dd60e1d895bed
TakaraAni <c324@kimo.com>
2021-12-27 14:13:16 +0800

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,7 +1,13 @@
 class main{
 	//主要運行通通從這邊進行
+	
+	/*************Setting_defult ur class project**************/
+	
+	/**********************************************************/
+	static int test = 12;
 	public static void main(String ar[]) {
+		
 		System.out.println("Hello");
-		System.out.println(D10919211.showMe());
+		System.out.println(D10919211.showMe() + "Anser = "+D10919211.Test(test));
 	}
 }
